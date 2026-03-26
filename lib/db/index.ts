f6cf4e6af950a99b1110799e4a7bdd7341d49c07
Vani <vanichaudhary0150@gmail.com>
2026-03-26 13:41:40 +0530

    build error fix

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -3,10 +3,5 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 neonConfig.fetchConnectionCache = true;
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("database url not found");
-}
-
-const sql = neon(process.env.DATABASE_URL);
-
+const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql);
